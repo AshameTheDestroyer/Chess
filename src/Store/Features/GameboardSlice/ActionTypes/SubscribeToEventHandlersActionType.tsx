1fbcc3269aa59a90ad7cd7da883a264fa552a2e5
Ashame The Destroyer <hashemwnoos@gmail.com>
2023-10-01 22:31:30 +0300

    Audio Manager

--- src/Store/Features/GameboardSlice/ActionTypes/SubscribeToEventHandlersActionType.tsx
+++
@@ -1,9 +0,0 @@
-import { GameboardEventHandlers } from "../GameboardSlice";
-import ArgumentType from "../../../../Utilities/Types/ArgumentType";
-
-type SubscribeToEventHandlersActionType<T extends keyof GameboardEventHandlers> = {
-    key: T;
-    subscriber: (...args: ArgumentType<GameboardEventHandlers[T][0]>) => ReturnType<GameboardEventHandlers[T][0]>;
-};
-
-export default SubscribeToEventHandlersActionType;
